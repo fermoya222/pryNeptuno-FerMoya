0fc59d26101e7810fb25bc09e9cd6a5fc82bed9a
fermoya222 <fermoya222@gmail.com>
2023-06-07 12:18:34 -0300

    cambios

---
+++ pryNeptuno-FerMoya/FrmBD.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace pryNeptuno_FerMoya
+{
+    partial class FrmBD
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConect = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConect
+            // 
+            this.btnConect.Location = new System.Drawing.Point(285, 56);
+            this.btnConect.Name = "btnConect";
+            this.btnConect.Size = new System.Drawing.Size(225, 9);
+            this.btnConect.TabIndex = 7;
+            this.btnConect.UseVisualStyleBackColor = true;
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9});
+            this.dgv.Location = new System.Drawing.Point(-72, 164);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(944, 231);
+            this.dgv.TabIndex = 6;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Column6";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Column7";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Column8";
+            this.Column8.Name = "Column8";
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Column9";
+            this.Column9.Name = "Column9";
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(364, 112);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(75, 23);
+            this.btnCargar.TabIndex = 5;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(339, 82);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(121, 13);
+            this.lblTitulo.TabIndex = 4;
+            this.lblTitulo.Text = "Cargar datos de clientes";
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.Location = new System.Drawing.Point(684, 416);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(66, 22);
+            this.btnSiguiente.TabIndex = 8;
+            this.btnSiguiente.Text = "Siguiente";
+            this.btnSiguiente.UseVisualStyleBackColor = true;
+            // 
+            // FrmBD
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSiguiente);
+            this.Controls.Add(this.btnConect);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.lblTitulo);
+            this.ForeColor = System.Drawing.Color.RosyBrown;
+            this.Name = "FrmBD";
+            this.Text = "FrmBD";
+            this.Load += new System.EventHandler(this.FrmBD_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConect;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnSiguiente;
+    }
+}
---
+++ pryNeptuno-FerMoya/FrmBD.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.Common;
+using System.Windows.Input;
+
+namespace pryNeptuno_FerMoya
+{
+    public partial class FrmBD : Form
+    {
+        OleDbCommand Command;
+        OleDbConnection Connection;
+        OleDbDataReader Reader;
+
+
+        public FrmBD()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmBD_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Connection = new OleDbConnection();
+
+                Connection.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=NEPTUNO.accdb";
+
+                Connection.Open();
+
+
+                btnConect.BackColor = Color.Green;
+
+            }
+            catch (Exception ex)
+            {
+                btnConect.Text = "Error";
+                btnConect.Text = ex.Message;
+                btnConect.BackColor = Color.Red;
+            }
+        }
+
+        private void clientes_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Command = new OleDbCommand();
+
+                Command.Connection = Connection;
+                Command.CommandType = CommandType.TableDirect;
+                Command.CommandText = "Clientes";
+
+                btnCargar.BackColor = Color.Green;
+
+                Reader = Command.ExecuteReader();
+
+                while (Reader.Read())
+                {
+
+                    dgv.Rows.Add(Reader[0], Reader[1], Reader[2], Reader[3], Reader[4], Reader[5], Reader[6], Reader[7], Reader[8], Reader[9]);
+
+                }
+
+
+            }
+            catch (Exception)
+            {
+                btnCargar.Text = "Error";
+                btnCargar.BackColor = Color.Red;
+
+
+            }
+        }
+
+}
